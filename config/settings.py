afcf83c0976f62b68eafe58d6e34180321100218
birinstore <huseyni823@gmail.com>
2024-08-27 20:40:44 +0330

    deploy

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['birinstore.com', '127.0.0.1',  'localhost']
 
 # Application definition
 
